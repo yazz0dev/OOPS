d0da18bf16a7fc15ab693388ec11f65aea6bf0fb
Yaseen k <yzac1569@gmail.com>
2025-03-28 14:15:31 +0530

    Add files via upload

---
+++ recbnry.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class recbnry {
+    static int recb(int[] arr, int l, int r, int x) {
+        if (r >= l) {
+            int mid = l + (r - l) / 2;
+            if (arr[mid] == x)
+                return mid;
+            if (arr[mid] > x)
+                return recb(arr, l, mid - 1, x);
+            return recb(arr, mid + 1, r, x);
+        }
+        return -1;
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements in the array");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        
+        System.out.println("Enter the elements of the array");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // Sort both arrays together
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap elements in both arrays
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        // Print the sorted array
+        System.out.println("Sorted array");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+        // Perform binary search
+        System.out.println("Enter the element to be searched");
+        int x = sc.nextInt();
+        int result = recb(arr, 0, n - 1, x);
+        if (result == -1)
+            System.out.println("Element not present in array");
+        else
+            System.out.println("Element found at index " + (result+1));
+    }
+}
