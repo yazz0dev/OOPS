8c42cb8e28c5713f15045e50b63cf47e266a4c0c
Yaseen k <yzac1569@gmail.com>
2025-06-11 13:18:13 +0530

    Update R12_mul.java

--- R12_mul.java
+++ R12_mul.java
@@ -1,47 +1,45 @@
 //12.Define 2 classes; one for generating multiplication table of 5 and other for displaying first N prime numbers.
 // Implement using threads.(Thread class) 
 
-import java.util.Scanner;
 class MultiplicationTable extends Thread {
     public void run() {
-        System.out.println("Multiplication Table of 5:");
-        for (int i = 1; i <= 10; i++) {
+       System.out.println("\nMultiplication Table of 5:");
+        for (int i = 1; i <= 10; i++)
             System.out.println("5 x " + i + " = " + (5 * i));
-        }
-    }
+       }
 }
 class PrimeNumbers extends Thread {
+    int n;
+    PrimeNumbers(int n) { this.n = n; }
     public void run() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the value of N: ");
-        int n = scanner.nextInt();
-        System.out.println("First " + n + " prime numbers:");
-        int count = 0;
-        int num = 2; // Start checking for prime from 2
+        System.out.println("\nFirst [N:" + n + "] prime numbers:");
+        int count = 0, num = 2;
         while (count < n) {
-            boolean isPrime = true;
-            for (int i = 2; i <= Math.sqrt(num); i++) {
-                if (num % i == 0) {
-                    isPrime = false;
-                    break;
-                }
-            }
-            if (isPrime) {
-                System.out.print(num + " ");
-                count++;
-            }
+            boolean prime = true;
+            for (int i = 2; i * i <= num; i++)
+                if (num % i == 0) { prime = false; break; }
+            if (prime) { System.out.print(num + " "); count++; }
             num++;
         }
-        System.out.println(); 
-        scanner.close();
+        System.out.println();
     }
 }
-public class R12_mul {
+public class sample {
     public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+
+        System.out.print("Enter Number N: ");
+        int n = sc.nextInt();
+
+        PrimeNumbers primeThread = new PrimeNumbers(n);
         MultiplicationTable tableThread = new MultiplicationTable();
-        PrimeNumbers primeThread = new PrimeNumbers();
-        
-        tableThread.start(); // Start the multiplication table thread
-        primeThread.start(); // Start the prime numbers thread
+
+        primeThread.start();
+        try { primeThread.join(); } catch (InterruptedException e) {}
+
+        tableThread.start();
+        try { tableThread.join(); } catch (InterruptedException e) {}
+
+        sc.close();
     }
 }
